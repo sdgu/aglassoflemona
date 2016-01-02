f60d1bddd935625b4a6430eb6c9718c1d74b23e1
Shawn <sdg18@duke.edu>
2016-01-01 17:24:35 -0800

    making scaling minimap easy

--- js/dota/beginner/minimapBack.js
+++ js/dota/beginner/minimapBack.js
@@ -5,6 +5,8 @@ var imageObj3 = new Image();
 var el = document.getElementById("reveal");
 el.onclick = drawAll;
 
+
+
 function drawAll()
 {
     context2.beginPath();
@@ -47,99 +49,81 @@ function drawAll()
 }
 
 
-//http://stackoverflow.com/questions/13627111/drawing-text-with-an-outer-stroke-with-html5s-canvas
-function drawStroked(text, x, y)
-{
-
-    context2.font = "12px Arial";
-    context2.strokeStyle = "black";
-    //context2.lineWidth = 4;
-    context2.strokeText(text, x, y);
-    context2.fillStyle = "white";
-    context2.fillText(text, x, y);
-    context2.miterLimit = 2;
-
-
-}
-
-function drawStroked2(cont, text, fSize, outWidth, outColor, inColor, x, y)
-{
-
-    cont.font = fSize + "px Arial";
-    cont.lineWidth = outWidth;
-    cont.strokeStyle = outColor;
-    //context2.lineWidth = 4;
-    cont.strokeText(text, x, y);
-    cont.fillStyle = inColor;
-    cont.fillText(text, x, y);
-    cont.miterLimit = 3;
-
-
-}
+    scaleBack = 1;
 
 imageObj3.onload = function()
 {
-    context2.drawImage(imageObj3, 0, 0, 209, 205);
+
+    context2.drawImage(imageObj3, 0, 0, 209 * scaleBack, 205 * scaleBack);
 
     canvas2.onclick = function(e)
     {
-        var rect = canvas2.getBoundingClientRect(),
-        x = e.clientX - rect.left,
-        y = e.clientY - rect.top;
-
-//var data = context2.getImageData(x, y, 1, 1).data;
-var xC = e.clientX - rect.left;
-var yC = Math.floor(e.clientY - rect.top);
-
-if ((xC > 9) && (xC < 25) && (yC > 179) && (yC < 195))
-{
-    context2.beginPath();
-    context2.arc(18, 188, 15, 0, 2 * Math.PI);
-
-    context2.lineWidth = 3;
-    context2.strokeStyle = "green";
-    context2.stroke();
-
-    drawStroked("Radiant Fountain", 34, 191);
+        var rect = canvas2.getBoundingClientRect();
 
 
-}
+        //var data = context2.getImageData(x, y, 1, 1).data;
+        var xC = e.clientX - rect.left;
+        var yC = Math.floor(e.clientY - rect.top);
 
-else if ((xC > 27) && (xC < 37) && (yC > 172) && (yC < 178))
-{
-    context2.beginPath();
-    context2.arc(33, 175, 5, 0, 2 * Math.PI);
 
-    context2.lineWidth = 3;
-    context2.strokeStyle = "#00e600";
-    context2.stroke();
+        if (isInsideRect(xC, yC, 9, 25, 179, 195, scaleBack))
+            //((xC > 9) && (xC < 25) && (yC > 179) && (yC < 195))
+        {
 
-    drawStroked("Radiant Ancient", 38, 179);
-}
 
-else if ((xC > 185) && (xC < 199) && (yC > 13) && (yC < 29))
-{
-	context2.beginPath();
-	context2.arc(192, 21, 15, 0, 2 * Math.PI);
+            drawCircle(context2, 18, 188, 15, 3, "green", scaleBack);
 
-	context2.lineWidth = 3;
-	context2.strokeStyle = "red";
-	context2.stroke();
+            // context2.beginPath();
+            // context2.arc(18, 188, 15, 0, 2 * Math.PI);
 
-	drawStroked("Dire Fountain", 214, 24);
-}
+            // context2.lineWidth = 3;
+            // context2.strokeStyle = "green";
+            // context2.stroke();
 
-else if ((xC > 173) && (xC < 181) && (yC > 37) && (yC < 43))
-{
-	context2.beginPath();
-    context2.arc(177, 40, 5, 0, 2 * Math.PI);
+            drawStroked2(context2, "Radiant Fountain", 12, 3, "black", "white", 35, 192, scaleBack);
 
-    context2.lineWidth = 3;
-    context2.strokeStyle = "#ff3333";
-    context2.stroke();
 
-    drawStroked("Dire Ancient", 186, 46);
-}
+        }
+
+        else if (isInsideRect(xC, yC, 26, 38, 169, 179, scaleBack)) 
+        //((xC > 26) && (xC < 38) && (yC > 169) && (yC < 179))
+        {
+
+            drawCircle(context2, 33, 175, 5, 3, "#00e600", scaleBack);
+
+            // context2.beginPath();
+            // context2.arc(33, 175, 5, 0, 2 * Math.PI);
+
+            // context2.lineWidth = 3;
+            // context2.strokeStyle = "#00e600";
+            // context2.stroke();
+
+            drawStroked2(context2, "Radiant Ancient", 12, 3, "black", "white", 38, 179, scaleBack);
+        }
+
+        else if ((xC > 185) && (xC < 199) && (yC > 13) && (yC < 29))
+        {
+        	context2.beginPath();
+        	context2.arc(192, 21, 15, 0, 2 * Math.PI);
+
+        	context2.lineWidth = 3;
+        	context2.strokeStyle = "red";
+        	context2.stroke();
+
+        	drawStroked("Dire Fountain", 214, 24);
+        }
+
+        else if ((xC > 172) && (xC < 182) && (yC > 36) && (yC < 44))
+        {
+        	context2.beginPath();
+            context2.arc(177, 40, 5, 0, 2 * Math.PI);
+
+            context2.lineWidth = 3;
+            context2.strokeStyle = "#ff3333";
+            context2.stroke();
+
+            drawStroked("Dire Ancient", 186, 46);
+        }
 
 
 
